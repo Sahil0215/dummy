4980ceea7e1cfef4552b45b73e4d9dff6295cf0a
Sahil0215 <sahilbhardwaj0215@gmail.com>
2024-08-26 11:34:53 +0530

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
+
+def home(request):
+    if request.method=="POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+
+        item = Item (name=name, phone=phone)
+
+        item.save()
+        redirect('home')
+    
+    item=Item.objects.all()
+    var = {
+        "item" : item
+    }
+    return render(request, 'home.html' , var)
 # Create your views here.
